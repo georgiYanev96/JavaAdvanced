333faacb625082d9fa748cb411d7f9492597dd13
georgiYanev96 <georgi.yanev.96@gmail.com>
2023-03-23 16:43:16 +0200

    I upload some of my exercises in which im using Stacks And Queues

--- src/StacksAndQueues/BrowserHistoryUpgrade.java
+++ src/StacksAndQueues/BrowserHistoryUpgrade.java
@@ -1,6 +1,8 @@
 package StacksAndQueues;
+
 import java.util.ArrayDeque;
 import java.util.Scanner;
+
 //Extend "Browser History" with a "forward" instruction which visits URLs that were navigated away from by "back"
 //Each forward instruction visits the next most-recent such URL.
 //If a normal navigation happens, all potential forward URLs are removed until a new back instruction is given
@@ -9,24 +11,34 @@ import java.util.Scanner;
 public class BrowserHistoryUpgrade {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String lastPage = "";
-        ArrayDeque<String> stack = new ArrayDeque<>();
-        while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("Home")) {
-                break;
-            }
-            if (input.equals("back") && lastPage.length() == 0) {
-                if (!stack.isEmpty()) {
-                    lastPage = stack.peek();
+        String input = scanner.nextLine();
+        ArrayDeque<String> stackURLs = new ArrayDeque<>();
+        ArrayDeque<String> forwardsQueue = new ArrayDeque<>();
+
+        while (!input.equals("Home")) {
+            if (input.equals("back")) {
+                if (stackURLs.size() < 2) {
+                    System.out.println("no previous URLs");
+                } else {
+                    String currentURL = stackURLs.pop();
+                    forwardsQueue.push(currentURL);
+                    System.out.println(stackURLs.peek());
                 }
-                System.out.println("no previous URLs");
-            } else if (input.equals("back") && lastPage.length() != 0) {
-                System.out.println(lastPage);
+            } else if (input.equals("forward")) {
+                if (forwardsQueue.isEmpty()) {
+                    System.out.println("no next URLs");
+                } else {
+                    String currentForward = forwardsQueue.pop();
+                    System.out.println(currentForward);
+                    stackURLs.push(currentForward);
+                }
             } else {
-                stack.push(input);
+                stackURLs.push(input);
                 System.out.println(input);
+                forwardsQueue.clear();
             }
+            input = scanner.nextLine();
         }
     }
 }
+
